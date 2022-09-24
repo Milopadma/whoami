5a7bbb3e17579738aa23dfa934229b14cf45ecaa
mylo <48256865+Milopadma@users.noreply.github.com>
2022-09-24 23:18:29 +0800

    cards are now children of modal component

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,20 +1,32 @@
-const Modal = ({ children }) => {
+const Modal = ({ children, title, description, link }) => {
   return (
     <div>
-      <label htmlFor="my-modal-4" className="btn modal-button">
-        open modal
+      <label htmlFor="my-modal-4" className="modal-button">
+        {children}
       </label>
 
       <input type="checkbox" id="my-modal-4" className="modal-toggle" />
       <label htmlFor="my-modal-4" className="modal cursor-pointer">
         <label className="modal-box relative" for="">
-          <h3 className="text-lg font-bold">
-            Congratulations random Internet user!
-          </h3>
-          <p className="py-4">
-            You've been selected for a chance to get one year of subscription to
-            use Wikipedia for free!
-          </p>
+          {/* banner iamge spanning the top 25% of the modal */}
+          <div className="modal-image">
+            <img
+              src="./portfolio.png"
+              alt=""
+              className="w-full h-full object-cover"
+            />
+          </div>
+
+          <h3 className="text-lg font-bold">{title}</h3>
+          <p className="py-4">{description}</p>
+          <a
+            href={link}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-blue-500"
+          >
+            View Project
+          </a>
         </label>
       </label>
     </div>
