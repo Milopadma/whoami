0cf10c9b36cc2ac72a8ea2424dba6f31c9b654ce
mylo <48256865+Milopadma@users.noreply.github.com>
2022-09-24 23:03:31 +0800

    installed daisyui and react, added new modal func

---
+++ src/components/Modal.jsx
@@ -0,0 +1,24 @@
+const Modal = ({ children }) => {
+  return (
+    <div>
+      <label htmlFor="my-modal-4" className="btn modal-button">
+        open modal
+      </label>
+
+      <input type="checkbox" id="my-modal-4" className="modal-toggle" />
+      <label htmlFor="my-modal-4" className="modal cursor-pointer">
+        <label className="modal-box relative" for="">
+          <h3 className="text-lg font-bold">
+            Congratulations random Internet user!
+          </h3>
+          <p className="py-4">
+            You've been selected for a chance to get one year of subscription to
+            use Wikipedia for free!
+          </p>
+        </label>
+      </label>
+    </div>
+  );
+};
+
+export default Modal;
