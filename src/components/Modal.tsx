291cee28379fcfacba85ed7986786aa36bce3e76
mylo <48256865+Milopadma@users.noreply.github.com>
2022-09-25 14:02:29 +0800

    properly working modals with content

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -31,6 +31,7 @@
 interface ModalProps {
   title: string;
   modalID: number;
+  tech: string;
   description: string;
   description2: string;
   description3: string;
@@ -46,13 +47,21 @@ export const Modal = (props: ModalProps) => {
         {props.children}
       </label>
 
-      <input type="checkbox" id={`my-modal-${props.modalID}`} className="modal-toggle" />
-      <label htmlFor={`my-modal-${props.modalID}`} className="modal cursor-pointer">
+      <input
+        type="checkbox"
+        id={`my-modal-${props.modalID}`}
+        className="modal-toggle"
+      />
+      <label
+        htmlFor={`my-modal-${props.modalID}`}
+        className="modal cursor-pointer"
+      >
         <label className="modal-box relative" htmlFor="">
           {/* banner iamge spanning the top 25% of the modal */}
-          <div className="modal-image">{props.image}</div>
+          <img src={props.image} alt={props.title} />
 
           <h3 className="text-2xl font-bold">{props.title}</h3>
+          <h4 className="text-l italic">Tech used: {props.tech}</h4>
           <p className="py-4">{props.description}</p>
           <p className="py-2">{props.description2}</p>
           <p className="py-2">{props.description3}</p>
