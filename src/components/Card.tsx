6fcb1427bd2da724611c8ba77728dfb7df7d64ff
mylo <48256865+Milopadma@users.noreply.github.com>
2022-09-26 22:39:26 +0800

    WIP - new courses section

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -5,28 +5,6 @@ export interface Props {
   href?: string;
 }
 
-// export const Card = ({ image, href, title, body }: Props) => {
-//   return (
-//     <div className="link-card">
-//       <a href={href} className="lg:min-h-[450px] min-h-0">
-//         <img
-//           className="max-h-[250px] min-h-[250px] w-full object-cover"
-//           src={image}
-//           alt={title}
-//           loading="lazy"
-//           width="100%"
-//           height="auto"
-//         />
-//         <h2 className="font-bold">
-//           {title}
-//           <span>&rarr;</span>
-//         </h2>
-//         <p className="italic">{body}</p>
-//       </a>
-//     </div>
-//   );
-// };
-
 export const Card = ({ image, title, body }: Props) => {
   return (
     <div>
--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,33 +1,3 @@
-// const Modal = () => {
-//   return (
-// <div>
-//   <label for="my-modal-4" class="modal-button">
-//     <slot/>
-//   </label>
-
-//   <input type="checkbox" id="my-modal-4" class="modal-toggle" />
-//   <label for="my-modal-4" class="modal cursor-pointer">
-//     <label class="modal-box relative" for="">
-//       {/* banner iamge spanning the top 25% of the modal */}
-//       <div class="modal-image">
-//         {image}
-//       </div>
-
-//       <h3 class="text-2xl font-bold">{title}</h3>
-//       <p class="py-4">{description}</p>
-//       <p class="py-2">{description2}</p>
-//       <p class="py-2">{description3}</p>
-
-//       <div class="flex justify-end items-center">
-//         {link}
-//       </div>
-//     </label>
-//   </label>
-// </div>
-//   );
-
-//   export default Modal;
-
 interface ModalProps {
   title: string;
   modalID: number;
