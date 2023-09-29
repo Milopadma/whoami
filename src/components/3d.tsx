fc9508da7e90141a9b8613a3082019201b807821
mylo <milopadma@yahoo.com>
2023-09-29 23:50:49 +0800

    it spins

--- src/components/3d.tsx
+++ src/components/3d.tsx
@@ -1,28 +1,44 @@
-import { Canvas } from "@react-three/fiber";
+import { Canvas, useFrame } from "@react-three/fiber";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { useLoader } from "@react-three/fiber";
+import { OrbitControls, OrthographicCamera } from "@react-three/drei";
+import React from "react";
 
 const Computers = ({ isMobile = false }) => {
   const gltf = useLoader(GLTFLoader, "/untitled.gltf");
+  const ref = React.useRef();
+  useFrame(() => (ref.current.rotation.y += 0.01));
 
   return (
     <group>
       <primitive
-        scale={isMobile ? 2 : 2}
+        ref={ref}
+        scale={isMobile ? 0.75 : 2}
         position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
         lightMapIntensity={0.5}
-        rotation={[-0.01, -0.2, -0.1]}
+        rotation={[-0.01, -2.5, -0]}
         object={gltf.scene}
       />
     </group>
   );
 };
 const ComputersCanvas = () => {
+  
   return (
-    <Canvas>
+    <Canvas id="canvas">
+      <pointLight position={[10, 10, 10]} />
+      <OrthographicCamera
+          makeDefault={false}
+          far={100000}
+          near={0}
+          position={[606.792, 751.53, -309.835]}
+          rotation={[-2.311, 0.565, 2.611]}
+        />
+      <OrbitControls />
       <Computers />
     </Canvas>
   );
 };
 
 export default ComputersCanvas;
+
